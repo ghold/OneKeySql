d3fbdac647eac60b797d4a93019beb3c72118104
Ghold <gholdnet@gmail.com>
2013-04-09 23:40:53 +0800

    use the Apache License

--- onekey/sql.py
+++ onekey/sql.py
@@ -14,13 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-class onekey(object):
-    sql_list = []
-    def __init__(self,  name,  desc=None):
+class sql(object):
+    def __init__(self,  name,  desc,  model):
         self.name = name
         self.desc = desc
+        self.model = model
         
-    def push(self,  sql):
-        self.sql_list.append(sql)
-        
-
+    def 
