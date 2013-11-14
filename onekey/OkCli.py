de81d68892650a06da47df19afe5c5083ecd1b9c
Ghold <gholdnet@gmail.com>
2013-11-14 15:45:39 +0800

    Cli complete

--- onekey/OkCli.py
+++ onekey/OkCli.py
@@ -3,53 +3,74 @@ from xml.sax import parse
 import OkXmlHandler
 from OkModel import OkModel
 from OkConfig import OkConfig
-import json
+from OkTagHandlerCli import OkTagHandler
+#import json
 import re
 from OkXmlHandler import OkTestcaseHandler
+from OkRuntime import OkExecProcess
 
 class OkDataParser(object):
     def __init__(self):
         self.cache = {}
+        self.sql = []
         self.data = {}
-        self.tag_position = {}
+        self.result = {}
         self.config = OkConfig()
         
-    def subTag(self, tags, data, spec):
+    def subTag(self, tags, data, result, spec):
+        option = result["option"]
+        var = result["var"]
         if spec:
             pass
         else:
-            print("VALUES( ")
+            self.sql.append("VALUES( ")
+            #print("VALUES( ")
         tag_pattern = r'\{[0-9a-zA-Z_]+\((?P<name>[0-9a-zA-Z_]+)\)\}'
         tag_compiler =re.compile(tag_pattern)
         tag_list = tag_compiler.split(data)
-        order = 0
         arg_pattern = r'\{(?P<name>[0-9a-zA-Z_]+)(?:|\((?P<arg>[+-]{1}[0-9]+)\))\}'
         arg_compiler = re.compile(arg_pattern)
         for val in tag_list:
             if tags.get(val) is not None:
                 arg_match = arg_compiler.match(tags[val])
                 if arg_match is not None:
-                    self.tag_position.setdefault(arg_match.group("name"), [])
-                    self.tag_position[arg_match.group("name")].append((order, arg_match.group("arg")))
-                print(tags[val])
-                order += 1
+                    name = arg_match.group("name")
+                    arg = arg_match.group("arg")
+                    value = self.config.callback(var[name][1])
+                    if value is not None:
+                        if arg is not None:
+                            method = var[name][0] + "_arg"
+                            value = OkTagHandler.callback(method, value, arg, var[name][1], self.config)
+                            self.config.reset()
+                            #print(value)
+                        self.sql.append(value)
+                    if option.get(name, None) is not None and var[name][2] is None:
+                        self.sql.append(option[name])
+                        #print(option[name])
+                else:
+                    self.sql.append(tags[val])
+                    #print(tags[val])
             else:
-                print(val)
+                self.sql.append(val)
+                #print(val)
                 
         if spec:
-            print(";\n")
+            self.sql.append(";\n")
+            #print(";\n")
         else:
-            print(");\n")
+            self.sql.append(");\n")
+            #print(");\n")
     
     #data setting
-    def setupData(self, data):
+    def setupData(self, data, result):
         self.data = data
+        self.result = result
         for step in range(0, len(data)):
             
             key =  "%s_%s" %(data[step]["type"], data[step]["from"])
             model = self.cache.get(key)
             
-            if  model is not None:
+            if model is not None:
                 pass
             else:
                 xml_filename = "%s/%s.xml" % (data[step]["type"], data[step]["from"])
@@ -61,14 +82,24 @@ class OkDataParser(object):
             data_id = data[step]["type"] + "_" + data[step]["data_id"]
             step_data = model.data['unit'][data_id]["data"]
             
-            #self.titleFormat(step + 1, step_data['desc'])
+            self.sql.append("/*Step %d：%s*/\n" % (step + 1, step_data['desc']))
             
             if (step_data.get("table", None) is not None and step_data.get("column", None) is not None):
                 tp_sql = "INSERT INTO\n%s(%s)\n" % (step_data["table"], step_data["column"])
-                print(tp_sql)
-                self.subTag(data[step]["tags"], step_data["value"], False)
+                #print(tp_sql)
+                self.sql.append(tp_sql)
+                self.subTag(data[step]["tags"], step_data["value"], result, False)
             else:
-                self.subTag(data[step]["tags"], step_data["value"], True)
+                self.subTag(data[step]["tags"], step_data["value"], result, True)
+                
+    def getSql(self):
+        return self.sql
+        
+    def sqlExec(self, data, result):
+        self.setupData(data, result)
+        thread = OkExecProcess(''.join(self.sql))
+        thread.start()
+        self.config.save()
 
 
 def isArgSet(data, options):
@@ -79,39 +110,39 @@ def isArgSet(data, options):
             option_dict.setdefault(op[0].strip(), op[1].strip())
     
     need = []
+    var = {}
     #match the form like {type_name(tag_name:default_val)}
-    tag_pattern = r"\{([0-9a-zA-Z_]+)\((?P<name>[0-9a-zA-Z_]+)(?:|\:(?P<def>.+))\)(?:|(?P<view>!))\}"
+    tag_pattern = r"\{(?P<type>[0-9a-zA-Z_]+)\((?P<name>[0-9a-zA-Z_]+)(?:|\:(?P<def>.+))\)(?:|(?P<view>!))\}"
     tag_compiler = re.compile(tag_pattern)
     
     for tag in data['data']['var'].split(','):
         result = tag_compiler.match(tag)
+        var.setdefault(result.group("name"), (result.group("type"), result.group("def"), result.group("view")))
         tmp_default_value = result.group("def")
         name = result.group("name")
         
         if tmp_default_value is None:
             if option_dict.get(name, None) is None:
                 need.append(name)
-                
-    return need
+    
+    return {"need":need, "var":var, "option":option_dict}
 
 
 def init(id, option):
-    print(option)
     handler = OkTestcaseHandler()
     parse('testcase/testcase.xml', handler)
     data = handler.getXmlData()
     parser = OkDataParser()
     key = "testcase_%s"%id
     
-    need = isArgSet(data[key], option)
-    if len(need) > 0:
-        print("You need to add %s"%' '.join(need))
+    result = isArgSet(data[key], option)
+    if len(result["need"]) > 0:
+        print("You need to add %s. Formatted as -o %s"%(' '.join(result["need"]), ','.join([s+':'+s.upper() for s in result["need"]])))
+        return
     
-    #jsonDumpsIndentStr = json.dumps(data[key], indent=1)
+    #jsonDumpsIndentStr = json.dumps(data[key]["data"], indent=1)
     #print(jsonDumpsIndentStr)
-    #parser.setupData(data[key]["data"]["steps"])
-    
-    
+    parser.sqlExec(data[key]["data"]["steps"], result)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
---
+++ onekey/OkTagHandlerCli.py
@@ -0,0 +1,81 @@
+from PyQt4 import QtCore
+class OkTagHandler(object):
+    @classmethod
+    def callback(cls,  name,  *args):
+        method = getattr(cls,  name,  None)
+        if callable(method):
+            return method(*args)
+        return None
+            
+#    @classmethod        
+#    def datetime(cls, *args):
+#        name, default, parent = args
+#        return QtCore.QDateTime.fromString(default, "yyyy-MM-dd hh:mm:ss")
+#        
+#    @classmethod
+#    def date(cls, *args):
+#        from OkEdit import OkDateEdit
+#        name, default, parent = args
+#        return OkDateEdit(name, default, parent)
+#        
+#    @classmethod        
+#    def time(cls, *args):
+#        from OkEdit import OkTimeEdit
+#        name, default, parent = args
+#        return OkTimeEdit(name, default, parent)
+#        
+#    @classmethod    
+#    def text(cls, *args):
+#        from OkEdit import OkTextEdit
+#        name, default, parent = args
+#        return OkTextEdit(name, default, parent)
+#        
+#    @classmethod    
+#    def increment(cls, *args):
+#        from OkEdit import OkIncrementEdit
+#        name, default, parent = args
+#        return OkIncrementEdit(name, default, parent)
+#        
+#    @classmethod
+#    def minute(cls, *args):
+#        from OkEdit import OkMinuteEdit
+#        name, default, parent = args
+#        return OkMinuteEdit(name, default, parent)
+        
+    @classmethod
+    def minute_arg(cls, *args):
+        number, arg, default, config = args
+        try:
+            return "%d"%(int(number) + int(arg))
+        except ValueError:
+            return number.strip("}") + "(" + arg + ")}"
+    
+    @classmethod
+    def datetime_arg(cls, *args):
+        datetime, arg, default, config = args
+        return QtCore.QDateTime.fromString(datetime, "yyyy-MM-dd hh:mm:ss").addMSecs(float(arg) * 1000 * 60).toString("yyyy-MM-dd hh:mm:ss")
+        
+    @classmethod
+    def date_arg(cls, *args):
+        date, arg, default, config = args
+        return QtCore.QDate.fromString(date, "yyyy-MM-dd").addDays(int(arg)).toString("yyyy-MM-dd")
+        
+    @classmethod
+    def time_arg(cls, *args):
+        time, arg, default, config = args
+        return QtCore.QTime.fromString(time, "hh:mm:ss").addMSecs(float(arg) * 1000 * 60).toString("hh:mm:ss")
+        
+    @classmethod    
+    def increment_arg(cls, *args):
+        number, arg, default, config = args
+        try:
+            if config.INCREMENT is None:
+                config.INCREMENT = number
+            config.INCREMENT = int(config.INCREMENT) + int(arg)
+            model = "%"+"0%dd"%len(number)
+            config.INCREMENT = model%config.INCREMENT
+            setattr(config, default, config.INCREMENT)
+            return config.INCREMENT
+        except ValueError:
+            return number.strip("}") + "(" + arg + ")}"
+    
